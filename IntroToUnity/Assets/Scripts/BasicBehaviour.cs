a9f2f8a73c2f3a2e6853f8174b4eec3eaf915149
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2019-12-16 13:42:21 -0500

    Update Intro
    Added the first steps to the project

---
+++ IntroToUnity/Assets/Scripts/BasicBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Text screenWords;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenWords.text = "Hello World";
+    }
+
+   
+}
